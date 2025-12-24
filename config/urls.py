88190b3dded4b67842ec4bfb9ad915700ad5c837
shahk-dev <shaxriyorturgunov000@gmail.com>
2025-12-24 22:15:59 +0500

    templeats

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , include('home.urls')),
     path('user/' , include('user.urls')),
-    path('products/' , include('products.urls'))
+    path('products/' , include('products.urls')),
 ]
